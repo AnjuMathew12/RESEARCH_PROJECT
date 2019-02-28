24f70f6b09a3c2627bc94ebe13caa58aa0ad818a
AnjuMathew12 <43873241+AnjuMathew12@users.noreply.github.com>
2019-02-28 16:32:10 +0530

    ASP.Net MVC Framework(28/02/2019)

---
+++ Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyProject.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Can't be empty")]
+        [Display(Name = "Student Name")]
+        //[RegularExpression()]
+
+        public string Sname { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [Display(Name = "Student address")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [Display(Name = "Email address")]
+        [DataType(DataType.EmailAddress)]
+        public string Semail { get; set; }
+
+        public Gender Sgender { get; set; }
+    }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+
+
+
+    }
+    
